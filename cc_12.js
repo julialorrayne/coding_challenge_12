30203d69233c12be6e21b61ccaaea90e0482b4a7
Julia <lorrayne@usf.edu>
2025-03-02 23:51:05 -0500

    Task 3 - Implemented Dynamic Inventory List

--- cc_12.js
+++ cc_12.js
@@ -24,4 +24,43 @@ metricCardArray.forEach(card => {
     card.style.backgroundColor = 'greenyellow';
 });
 
+//Task 3: Dynamic Inventory Management – Adding and Removing Items
+const inventoryList = document.getElementById('inventoryList');
 
+function addInventoryItem(name,quantity) {
+    let newLi = document.createElement('li');
+    newLi.setAttribute('class','product-item');
+    newLi.setAttribute('dataset',product);
+    newLi.addEventListener('click',() => {removeInventoryItem(newLi)});
+    newLi.textContent = Product;
+    inventoryList.appendChild(newLi);
+};
+
+addInventoryItem("Click me");
+addInventoryItem("Add a product below");
+
+let productForm = document.getElementById('productForm');
+let error = document.getElementById('error');
+
+productForm.addEventListener('submit', (event) => {
+    let productName = document.getElementById('productName').value;
+    if (productName.trim() === '') {
+        error.textContent = 'Product name is required.';
+        event.preventDefault();
+    } else {
+        error.textContent = '';
+        addInventoryItem(productName);
+        event.preventDefault();
+    }
+});
+
+function removeInventoryItem(item) {
+    inventoryList.removeChild(item);
+}
+
+
+
+// Write a function that creates a new <li> element representing a product.
+// Use setAttribute to add a class (e.g., "product-item") or a custom data attribute to the <li>.
+// Append the new product item to the inventory list using appendChild.
+// Write a separate function that removes a specific <li> when it is clicked. Use removeChild within the inventory list to remove the clicked item.
